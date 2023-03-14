5681f766626bf55bc23625f158f2cadd106a2094
bhavy-deshani <deshanibhavya1730@gmail.com>
2023-03-14 12:09:13 +0530

    PSD

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+    //spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+})
